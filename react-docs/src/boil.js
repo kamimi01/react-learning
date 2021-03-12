110756b9bfcde294cffe97fb541d8fbe24cc1c7c
kamimi01 <awakaru0122@gmail.com>
2021-03-12 21:51:36 +0900

    feat: stateのリフトアップの学習完了

---
+++ react-docs/src/boil.js
@@ -0,0 +1,115 @@
+// stateのリフトアップとは↓
+// コンポーネント間でstateの共有をしたい場合は、
+// それを必要とするコンポーネント全ての直近の共通祖先コンポーネントに移動する
+
+/**
+ * 水が沸騰するのに、十分な温度かどうかを表示する
+ * @param {celsius} props
+ */
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+const scaleNames = {
+  c: "Celsuis",
+  f: "Fahrenheit",
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+/**
+ * 摂氏から華氏に変換するための関数2つ
+ */
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+/**
+ * 常に少数第3位までで四捨五入し、無効な温度には空の文字列を返す
+ */
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+/**
+ * 温度を入力するための<input>要素のレンダーと
+ * 入力された値をstateの値として保持する
+ */
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsuisChange = this.handleCelsuisChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
+
+  handleCelsuisChange(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsuisChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById("boil"));
